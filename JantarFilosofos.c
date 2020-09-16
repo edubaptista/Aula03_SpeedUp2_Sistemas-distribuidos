7ac72a34ce3215baff7dced4a65cb3e54cc1e064
edubaptista <43965828+edubaptista@users.noreply.github.com>
2020-09-15 22:01:41 -0300

    Update JantarFilosofos.c

--- JantarFilosofos.c
+++ JantarFilosofos.c
@@ -11,8 +11,13 @@
 #define LEFT (ph_num + N_PHILOSOPHERS - 1) % N_PHILOSOPHERS
 #define RIGHT (ph_num + 1) % N_PHILOSOPHERS
 
+#define tPensando 0.05
+#define tComendo 0.1
+
 pthread_mutex_t mutex;
 pthread_cond_t condition[N_PHILOSOPHERS];
+
+int cycle = 0;
 
 enum { THINKING, HUNGRY, EATING } state[N_PHILOSOPHERS];
 int phil_num[N_PHILOSOPHERS];
@@ -34,12 +39,12 @@ int main(int argc, char *argv[])
     phil_num[i] = i;		
   }
 
-
   /* Meat */
 
   for (i = 0; i < N_PHILOSOPHERS; i++) {
     pthread_create(&ph_thread[i], NULL, philosophing, &phil_num[i]);
-    printf("Philosopher #%d sits on the table.\n", i + 1);
+    cycle += 1;
+    printf("Cycle %d - Philosopher #%d sits on the table.\n", cycle, i + 1);
     sleep(1);
   }
   for (i = 0; i < N_PHILOSOPHERS; i++)
@@ -57,29 +62,32 @@ void *philosophing(void *arg)
 {
   while(1) {
     int *ph_num = arg;
-    printf("Philosopher #%d starts thinking.\n", *ph_num + 1);
-    sleep(2);
+    cycle += 1;
+    printf("Cycle %d - Philosopher #%d starts thinking.\n", cycle, *ph_num + 1);
+    sleep(tPensando);
     pickup_forks(*ph_num);
     return_forks(*ph_num);
   }
 }
 
 void pickup_forks(int ph_num) {
-  pthread_mutex_lock(&mutex);
-
-  printf("Philosopher #%d is HUNGRY. She tries to grab her forks.\n", ph_num + 1);
+  //pthread_mutex_lock(&mutex);
+  cycle += 1;
+  printf("Cycle %d - Philosopher #%d is HUNGRY. She tries to grab her forks.\n", cycle, ph_num + 1);
   state[ph_num] = HUNGRY;
   test(ph_num);
-  //while (state[ph_num] != EATING) 
-    pthread_cond_wait(&condition[ph_num], &mutex);
+  while (state[ph_num] != EATING) 
+  	pthread_cond_wait(&condition[ph_num], &mutex);
 
-  pthread_mutex_unlock(&mutex);
+   //pthread_mutex_unlock(&mutex);
 }
 
 void return_forks(int ph_num) {
   pthread_mutex_lock(&mutex);
 
-  printf("Philosopher #%d puts down chopsticks. Now she asks her neighbors if they are hungry.\n", ph_num + 1);
+  cycle += 1;
+  printf("Cycle %d - Philosopher #%d puts down chopsticks. Now she asks her neighbors if they are hungry.\n", cycle, ph_num + 1);
+
   state[ph_num] = THINKING;
   test(LEFT);
   test(RIGHT);
@@ -91,9 +99,10 @@ void test(int ph_num) {
   if (state[ph_num] == HUNGRY && 
       state[LEFT] != EATING && 
       state[RIGHT] != EATING) {
-    printf("Philosopher #%d starts EATING.\n", ph_num + 1);
+    cycle += 1;
+    printf("Cycle %d - Philosopher #%d starts EATING.\n", cycle, ph_num + 1);
     state[ph_num] = EATING;
-    sleep(3);
+    sleep(tComendo);
     pthread_cond_signal(&condition[ph_num]);
   }
 }
